579486f80ec8babe8268bd2ee0657aa4a97cb456
JavierGVA <javrodlop_3@alu.edu.gva.es>
2023-10-11 09:54:06 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            contador.Text = (cajaTexto.Text.Length).ToString();
+
+            if (cajaTexto.Text.Length >= 140)
+            {
+               
+                cajaTexto.IsReadOnly = true;
+            }
+            else
+            {
+                
+                cajaTexto.IsReadOnly = false;
+            }
+
+        }
+
+        
+    }
+}
